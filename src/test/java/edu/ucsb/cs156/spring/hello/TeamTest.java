90ede899940dec8aeace6a462fa5920d986d6985
jzipkin1812 <javinzipkin@gmail.com>
2024-10-09 10:00:19 -0700

    team tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,42 @@ public class TeamTest {
     @Test
     public void toString_returns_correct_string() {
         assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_object_returns_true() {
+        Team teamA = new Team( "equal-team");
+        assertTrue(teamA.equals(teamA));
+    }
+
+    @Test
+    public void same_attributes_returns_true() {
+        // T T
+        Team teamA = new Team( "equal-team");
+        Team teamB = new Team( "equal-team");
+        assertTrue(teamA.equals(teamB));
+    }
+
+    @Test
+    public void equals_different_class_returns_false() {
+        assertTrue(!(team.equals(5)));
+    }
+
+    @Test
+    public void equals_different_attributes_returns_false() {
+        Team teamA = new Team( "almostequal-team");
+        Team teamB = new Team( "almostequal-team");
+        teamB.addMember("javin");
+        teamA.addMember("maya");
+        // T F
+        assertTrue(!(teamA.equals(teamB)));
+        // F F
+        teamA.setName("random-team");
+        assertTrue(!(teamA.equals(teamB)));
+        // F T
+        teamA = new Team("random-team");
+        teamB = new Team("superrandom-team");
+        assertTrue(!(teamA.equals(teamB)));
     }
 
 }
